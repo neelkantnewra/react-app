27af5c946b12ba802ecab5ca30cc9c19797ce85d
Neelkant Newra (नीलकांत नेवरा) <newra008@gmail.com>
2022-09-26 07:00:38 +0000

    commit v1.0

---
+++ app1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='top-nav-bar'>
+       <div className='brand'>Deep-Cart</div> 
+       <div className='sub-nav'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
