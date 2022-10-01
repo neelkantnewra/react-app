b85974b3cfa7edec1a51cb63b0ba2fbb04db0864
Neelkant Newra (नीलकांत नेवरा) <newra008@gmail.com>
2022-10-01 04:05:16 +0000

    app2 added

---
+++ app2/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+    <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+      Sign in
+    </a>
+    <a
+      href="#"
+      className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+    >
+      Sign up
+    </a>
+  </div>
+  );
+}
+
+export default App;
